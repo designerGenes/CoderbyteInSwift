bf50faaf4b3d3b5d61fb6fd260f89fa45952f3d7
Jaden Nation <jnationdesignerjeans@gmail.com>
2019-04-15 17:42:11 -0700

    add DashInsert2

---
+++ DashInsert2/DashInsert2.playground/Contents.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+/*
+ Have the function DashInsertII(str)
+
+ 1. insert dashes ('-') between each two odd numbers and
+ 2. insert asterisks ('*') between each two even numbers in str.
+ 
+ For example: if str is 4546793 the output should be 454*67-9-3.
+ Don't count zero as an odd or even number.
+ */
+
+func dashInsertII(raw: String) -> String {
+    let rawIntArr = raw.compactMap({Int(String($0))})
+    var out = [String]()
+    (0..<rawIntArr.count - 1).forEach { k in
+        let l = rawIntArr[k]
+        let r = rawIntArr[k + 1]
+        let matchingEvens = (l % 2 + r % 2) < 1 && (l != 0 && r != 0)
+        let matchingOdds = (l % 2 + r % 2) > 1 && (l != 0 && r != 0)
+        out.append(String(rawIntArr[k]) + (matchingEvens ? "*" : matchingOdds ? "-" : ""))
+    }
+    out.append(String(rawIntArr[rawIntArr.count - 1]))
+    return out.reduce("", +)
+}
+
+print(dashInsertII(raw: "56647304"))
